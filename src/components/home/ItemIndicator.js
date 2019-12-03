c96651dc1155c3b716060fa2643c1d162fcdc610
Alan Bandeira <alan.p.bandeira@gmail.com>
2019-12-03 19:54:11 -0200

    small fix

---
+++ src/components/home/ItemIndicator.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+
+export default class ItemIndicator extends React.Component {
+
+  generateItemIdx = (numItems) => {
+    
+  }
+
+  render() {
+    return (
+      <ol>
+        
+      </ol>
+    );
+  }
+}
